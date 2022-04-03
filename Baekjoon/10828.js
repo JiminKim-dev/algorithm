ee99bf32d24227bdf514f66c7fcb00779d65ba3c
jm <jm.dev.study@gmail.com>
2022-04-03 23:38:48 +0900

    [Baekjoon] 10828_스택

---
+++ Baekjoon/10828.js
@@ -0,0 +1,38 @@
+// 스택
+
+const fs = require('fs'); 
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+function solution(arr) {
+  const stack = [];
+  const result = [];
+  
+  arr.forEach(el => {
+    const [command, num] = el.split(' ')
+    
+    switch(command) {
+      case 'push': 
+        stack.push(num)
+        break
+      case 'pop':
+        stack.length !== 0 ? result.push(stack.pop()) : result.push('-1')
+        break;
+      case 'size':
+        result.push(stack.length);
+        break
+      case 'top': 
+        stack.length === 0 ? result.push('-1') : result.push(stack[stack.length -1]);
+        break;
+      case 'size': 
+        result.push(stack.length)
+        break
+      case 'empty':
+        stack.length === 0 ? result.push('1') : result.push('0')
+        break;
+    }
+  })
+
+  return result.join('\n');
+}
+
+console.log(solution(input));
